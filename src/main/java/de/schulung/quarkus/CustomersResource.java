9a2d5a5874429afdb261d9baebb30f5236db1389
Ralf Ueberfuhr <ralf.ueberfuhr@atvantage.com>
2026-05-19 14:36:12 +0200

    Add test and endpoint for `POST /customers`

--- src/main/java/de/schulung/quarkus/CustomersResource.java
+++ src/main/java/de/schulung/quarkus/CustomersResource.java
@@ -1,12 +1,15 @@
 package de.schulung.quarkus;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
 @Path("/customers")
 public class CustomersResource {
@@ -15,6 +18,16 @@ public class CustomersResource {
   @Produces(MediaType.APPLICATION_JSON)
   public Collection<?> getCustomers() {
     return List.of();
+  }
+
+  @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response createCustomer(Customer customer) {
+    customer.setUuid(UUID.randomUUID());
+    return Response
+      .status(Response.Status.CREATED)
+      .entity(customer)
+      .build();
   }
 
 }
--- src/test/java/de/schulung/quarkus/CustomerApiTests.java
+++ src/test/java/de/schulung/quarkus/CustomerApiTests.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 @QuarkusTest
 class CustomerApiTests {
@@ -38,6 +40,36 @@ class CustomerApiTests {
       .then()
       .statusCode(406);
   }
+
+  @Test
+  void when_post_customers_then_status_created_and_json_response() {
+    given()
+      .contentType(ContentType.JSON)
+      .body("""
+            {
+              "name": "Tom Mayer",
+              "birthdate": "2020-05-19",
+              "state": "active"
+            }
+        """)
+      .accept(ContentType.JSON)
+      .when()
+      .post("/customers")
+      .then()
+      .statusCode(201)
+      .contentType(ContentType.JSON)
+      .body("name", is(equalTo("Tom Mayer")))
+      .body("birthdate", is(equalTo("2020-05-19")))
+      .body("state", is(equalTo("active")))
+      .body("uuid", is(notNullValue()));
+  }
+
+  // TODO: Content Negotiation: invalid Accept -> 406, invalid Content-Type -> 415
+  // TODO: when created with UUID, use UUID to request for single customer
+  // TODO: Location-Header exists and contains URL to new resource
+  // TODO: when requesting a non-existing customer, 404 is returned
+  // TODO: invalid accept header for single customer request -> 406
+  // TODO: when creating a customer, a UUID must not be sent with the request body -> 400
 
 
 }
