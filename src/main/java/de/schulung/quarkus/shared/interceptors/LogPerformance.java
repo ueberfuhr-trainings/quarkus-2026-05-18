844964a96dfd9df310f3be638ac1927ab4dae36e
Ralf Ueberfuhr <ralf.ueberfuhr@atvantage.com>
2026-05-22 14:15:28 +0200

    Introduce interceptor to log method performance (NO TEST)

---
+++ src/main/java/de/schulung/quarkus/shared/interceptors/LogPerformance.java
@@ -0,0 +1,18 @@
+package de.schulung.quarkus.shared.interceptors;
+
+import jakarta.interceptor.InterceptorBinding;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Documented
+@Inherited
+@InterceptorBinding
+public @interface LogPerformance {
+}
